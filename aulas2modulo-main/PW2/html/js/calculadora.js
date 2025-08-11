e2caafe24a848bc43aa206e422e9539883cc869b
GaBMarqz <gabriel.marques107@etec.sp.gov.br>
2025-08-11 18:59:19 -0300

    Add files via upload

---
+++ aulas2modulo-main/PW2/html/js/calculadora.js
@@ -0,0 +1,6 @@
+var numero1 = parseInt(prompt("Insira um número: "));
+var numero2 = parseInt(prompt("Insira outro número: "));
+var soma = numero1 + numero2;
+
+console.log(soma);
+alert(soma);
---
+++ aulas2modulo-main/README.md
@@ -0,0 +1,3 @@
+# aulas2modulo
+
+essa pasta vai ser sobre os arquivos das aulas do 2.modulo do curso de Desenvolvimento de Sistemas.
