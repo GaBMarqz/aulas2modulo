c51729054b5750ea7de8a42318d6ba140716e70e
GaBMarqz <gabriel.marques107@etec.sp.gov.br>
2025-08-27 20:30:14 -0300

    Add files via upload

---
+++ DS/Java/Scanner/src/Exercicios/idadeternario.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+public class idadeternario {
+    public static void main(String[] args) {
+        
+        int idade = 18;
+        String resultado = (idade <= 18) ? "Maior de idade" : "Menor de idade";
+
+        System.out.println(resultado);
+        
+    }
+    
+}
---
+++ DS/Java/Scanner/src/Exercicios/parouimparcomrestodediv.java
@@ -0,0 +1,13 @@
+package Exercicios;
+
+public class parouimparcomrestodediv {
+    public static void main(String[] args) {
+        int numero = 24;
+
+        if(numero % 2 == 0){
+            System.out.println("Par!");
+        }else{
+            System.out.println("Impar!");
+        }
+    }
+}
