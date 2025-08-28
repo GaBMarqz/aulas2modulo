74b84755d36e7cbdedd84962b2509f8db293652c
GaBMarqz <gabriel.marques107@etec.sp.gov.br>
2025-08-27 22:25:09 -0300

    Add files via upload

---
+++ DS/Java/Scanner/src/Instanciar/Instanciar/Calculdadora.java
@@ -0,0 +1,120 @@
+package Instanciar;
+
+import java.util.Scanner;
+
+public class Calculdadora {
+
+    Scanner calc = new Scanner(System.in);
+    
+    String nome;
+    double altura;
+
+    double a;
+    double b;
+    double res;
+
+    void dados(){
+
+        System.out.println("Digite o seu nome: ");
+        nome = calc.nextLine();
+        System.out.println("Digite a sua altura: ");
+        altura = calc.nextDouble();       
+
+        //nome = "Gabriel";
+        //altura = 1.74;
+
+        System.out.println("Seu nome é: " + nome + 
+                                "Sua altura é: " + altura);
+    }
+    
+    void somar(){
+
+        System.out.println("Digite o item A: ");
+        a = calc.nextDouble();
+
+        System.out.println("Digite o item B:");
+        b = calc.nextDouble();
+
+         //double a = 10;
+         //double b = 10;
+         //double res = a + b;
+
+         double res = a + b;
+         System.out.println("O valor da soma é: " + res);
+
+}
+void subtrair(){
+
+    System.out.println("Digite o item A: ");
+    a = calc.nextDouble();
+
+    System.out.println("Digite o item B:");
+    b = calc.nextDouble();
+
+     //double a = 10;
+     //double b = 10;
+     //double res = a - b;
+
+     double res = a - b;
+     System.out.println("O valor da soma é: " + res);
+
+}
+
+void multiplicar(){
+
+    System.out.println("Digite o item A: ");
+    a = calc.nextDouble();
+
+    System.out.println("Digite o item B:");
+    b = calc.nextDouble();
+
+     //double a = 10;
+     //double b = 10;
+     //double res = a * b;
+
+     double res = a * b;
+     System.out.println("O valor da soma é: " + res);
+
+}
+
+void dividir(){
+
+    System.out.println("Digite o item A: ");
+    a = calc.nextDouble();
+
+    System.out.println("Digite o item B:");
+    b = calc.nextDouble();
+
+     //double a = 10;
+     //double b = 10;
+     //double res = a / b;
+
+     double res = a / b;
+     System.out.println("O valor da soma é: " + res);
+
+}
+
+void areaTriangulo(){
+
+    System.out.println("Digite o item A: ");
+    a = calc.nextDouble();
+
+    System.out.println("Digite o item B:");
+    b = calc.nextDouble();
+
+     double res = (a * b)/2;
+     System.out.println("O valor da soma é: " + res);
+}
+
+void areaQuadrado(){
+
+    System.out.println("Digite o item A: ");
+    a = calc.nextDouble();
+
+    System.out.println("Digite o item B:");
+    b = calc.nextDouble();
+
+     double res = a * a;
+     System.out.println("O valor da soma é: " + res);
+}
+}
---
+++ DS/Java/Scanner/src/Instanciar/Instanciar/teste.java
@@ -0,0 +1,20 @@
+package Instanciar;
+
+public class teste {
+    
+public static void main(String[] args) {
+        
+    Calculdadora calc = new Calculdadora();
+
+    calc.dados();
+    calc.somar();
+    calc.subtrair();
+    calc.multiplicar();
+    calc.dividir();
+    calc.areaTriangulo();
+    calc.areaQuadrado();
+
+}
+}
+
+    
