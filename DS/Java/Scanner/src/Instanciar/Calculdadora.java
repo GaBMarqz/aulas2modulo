17daa947ce2df401e13c0de390fb567ba5a26690
GaBMarqz <gabriel.marques107@etec.sp.gov.br>
2025-08-27 21:56:20 -0300

    Add files via upload

---
+++ DS/Java/Scanner/src/Instanciar/Calculdadora.java
@@ -0,0 +1,46 @@
+package Instanciar;
+
+import java.util.Scanner;
+
+public class Calculdadora {
+
+    Scanner calc = new Scanner(System.in);
+    
+    String nome;
+    double altura;
+
+    double a;
+    double b;
+    double res;
+
+    void dados(){
+
+        System.out.println("Digite o seu nome: ");
+        nome = calc.nextLine();
+        System.out.println("Digite a sua altura: ");
+        altura = calc.nextDouble();       
+
+        //nome = "Gabriel";
+        //altura = 1.74;
+
+        System.out.println("Seu nome é: " + nome + 
+                                "Sua altura é: " + altura);
+    }
+    
+    void somar(){
+
+        System.out.println("Digite o item A: ");
+        a = calc.nextDouble();
+
+        System.out.println("Digite o item B:");
+        b = calc.nextDouble();
+
+         //double a = 10;
+         //double b = 10;
+         //double res = a + b;
+
+         System.out.println("O valor da soma é: " + res);
+
+}
+
+}
---
+++ DS/Java/Scanner/src/Instanciar/teste.java
@@ -0,0 +1,15 @@
+package Instanciar;
+
+public class teste {
+    
+public static void main(String[] args) {
+        
+    Calculdadora calc = new Calculdadora();
+
+    calc.dados();
+    calc.somar();
+
+}
+}
+
+    
