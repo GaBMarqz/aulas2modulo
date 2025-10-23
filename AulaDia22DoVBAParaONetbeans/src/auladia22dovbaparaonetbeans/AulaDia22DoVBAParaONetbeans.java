6a7f6693ae7bde97ea5dbf560a38e8a18e12eb62
GaBMarqz <gabriel.marques107@etec.sp.gov.br>
2025-10-22 22:18:41 -0300

    Add files via upload

---
+++ AulaDia22DoVBAParaONetbeans/src/auladia22dovbaparaonetbeans/AulaDia22DoVBAParaONetbeans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package auladia22dovbaparaonetbeans;
+
+/**
+ *
+ * @author GABRIEL_4964
+ */
+public class AulaDia22DoVBAParaONetbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
